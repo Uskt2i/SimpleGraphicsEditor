059672504ef11f9eda814d5cb438523db0a0f593
Uskt2i <yusuketanicg@gmail.com>
2020-05-15 20:24:17 +0900

    v5
    gamma adjust.
    MessageBox doesn't work

--- OpencvProcessing.py
+++ OpencvProcessing.py
@@ -15,12 +15,19 @@ class OpencvProcessing:
         pic=cv_file*gain
         pic=np.clip(pic,0,255).astype(np.uint8)
         return pic
+    def gamma(self,cv_file,gamma=1.0):
+        gamma_cvt = np.zeros((256,1),dtype = 'uint8')
+        for i in range(256):
+            gamma_cvt[i][0] = 255 * (float(i)/255) ** (1.0/gamma)
+        pic = cv2.LUT(cv_file,gamma_cvt)
+        return pic
 
 if __name__=="__main__":
     open_file= "D:\python\dev\ImageProcessing\image\DSC_0104_FHDhalf.jpg"
     cvtest= OpencvProcessing()
     img= cvtest.openPic(open_file)
     img= cvtest.gain(img,1)
+    img= cvtest.gamma(img,0.5)
     img= cvtest.convertPic(img)
 
     plt.imshow(img)
--- SimpleGraphicsEditor.py
+++ SimpleGraphicsEditor.py
@@ -19,35 +19,54 @@ class UIGraphicsEditorWidget(QtWidgets.QMainWindow):
         self.ui = QUiLoader().load(os.path.join(CURRENT_PATH, 'SimpleGraphicsEditor.ui'))
         self.setCentralWidget(self.ui)
         self.setWindowTitle("Simple Graphics Editor")
+        self.setGeometry(200,200,640,640)
         self.setCSS()
 
         self.ui.lineEditGain.setText('1.0')
+        self.ui.lineEditGamma.setText('1.0')
+        self.ui.graphicsView.resize(800,600)
         # Signal作成
         self.ui.pushButtonExcute.clicked.connect(self.clickbtn)
 
         self.ui.horizontalSliderGain.valueChanged.connect(self.sliderValueChangeGain)
-        self.ui.lineEditGain.textChanged.connect(self.changeText)
+        self.ui.lineEditGain.textChanged.connect(self.changeTextGain)
+
+        self.ui.horizontalSliderGamma.valueChanged.connect(self.sliderValueChangeGamma)
+        self.ui.lineEditGamma.textChanged.connect(self.changeTextGamma)
 
         self.ui.action_Open.triggered.connect(self.open)
         self.ui.action_Save.triggered.connect(self.save)
         self.ui.actionExit.triggered.connect(self.exit)
         self.ui.actionAbout.triggered.connect(self.about)
+
     def sliderValueChangeGain(self):
         view_num=float(self.ui.horizontalSliderGain.value())/100
         self.ui.lineEditGain.setText(str(view_num))
-    def changeText(self,text):
+    def sliderValueChangeGamma(self):
+        view_num=float(self.ui.horizontalSliderGamma.value())/100
+        self.ui.lineEditGamma.setText(str(view_num))
+    def changeTextGain(self,text):
+        edit_num=float(text)*100
+        #self.ui.horizontalSliderGain.value(int(edit_num))
+    def changeTextGamma(self,text):
         edit_num=float(text)*100
         #self.ui.horizontalSliderGain.value(int(edit_num))
     def clickbtn(self):
 
         gain_value=float(self.ui.lineEditGain.text())
+        gamma_value=float(self.ui.lineEditGamma.text())
+        print(gain_value)
         cv_test=OpencvProcessing()
         self.adjustimg=cv_test.gain(self.img,gain_value)
+        self.adjustimg=cv_test.gamma(self.adjustimg,gamma_value)#self.adjusting大事
         self.updataImage(self.adjustimg)
 
     def open(self):
         self.file = QtWidgets.QFileDialog().getOpenFileName()
         self.img=cv2.imread(self.file[0])
+        height,width,channels=self.img.shape
+        print(height,width,channels)
+        #self.ui.graphicsView.resize(height,width)
         self.updataImage(self.img)
 
     def updataImage(self,updata_img):
@@ -57,6 +76,7 @@ class UIGraphicsEditorWidget(QtWidgets.QMainWindow):
         Qtimage=QtGui.QImage(convert_img.data,width,height,bytesPerLine, QtGui.QImage.Format_RGB888)
         scene=QtWidgets.QGraphicsScene()
         scene.addPixmap(QtGui.QPixmap.fromImage(Qtimage))
+        #scene.width(width)
         self.ui.graphicsView.setScene(scene)
 
     def save(self):
@@ -65,7 +85,7 @@ class UIGraphicsEditorWidget(QtWidgets.QMainWindow):
         print(file_name)
         save_file=file_name[0]
         cv2.imwrite(save_file,self.adjustimg)
-        QMessageBox.information(self, "Message", "Save File [ "+save_file+" ]")
+        QMessageBox.information(self, "Message", "Save File")
 
     def exit(self):
         self.close()
